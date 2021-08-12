1b78c510b37251157a2ecad97853f6cb6eb6bf6f
n1rdeks <n1rdeks@mail.ru>
2021-08-12 19:47:27 +0300

    added localstorage

--- movie-seat-booking/script.js
+++ movie-seat-booking/script.js
@@ -3,16 +3,48 @@ const seats = document.querySelectorAll('.row .seat:not(.occupied)')
 const count = document.getElementById('count')
 const total = document.getElementById('total')
 const movieSelect = document.getElementById('movie')
+
+
+populateUI()
+
 let ticketPrice = parseInt(movieSelect.value)
+
+const setMovieDataLS = (movieIndex, moviePrice) => {
+  localStorage.setItem('selectedMovieIndex', movieIndex)
+  localStorage.setItem('selectedMoviePrice', moviePrice)
+}
 
 const updateSelectedCount = () => {
   const selectedSeats = document.querySelectorAll('.row .seat.selected')
+
+  const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat))
+
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex))
   count.innerText = selectedSeats.length
   total.innerText = selectedSeats.length * ticketPrice
 }
 
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'))
+  
+  if(selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if(selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('selected')
+      }
+    })
+  }
+
+  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex')
+
+  if(selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex
+  }
+}
+
 movieSelect.addEventListener('change', e => {
   ticketPrice = parseInt(e.target.value)
+  setMovieDataLS(e.target.selectedIndex, e.target.value)
   updateSelectedCount()
 })
 
@@ -23,3 +55,5 @@ container.addEventListener('click', e => {
 
   updateSelectedCount()
 })
+
+updateSelectedCount()
