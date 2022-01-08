7211f59ccdc3616e59396c7419fbc41dd37fdff7
n1rdeks <n1rdeks@mail.ru>
2022-01-08 21:18:32 +0300

    add fetch

---
+++ dom_array_methods/script.js
@@ -0,0 +1,46 @@
+const mainBtn = document.getElementById('main')
+const addUserBtn = document.getElementById('add-user')
+const doubleBtn = document.getElementById('double')
+const showBtn = document.getElementById('show-millionaires')
+const sortBtn = document.getElementById('sort')
+const calcBtn = document.getElementById('calculate-wealth')
+
+let data = []
+
+getRandomUser()
+getRandomUser()
+getRandomUser()
+
+// fetch random user and add money
+async function getRandomUser() {
+    const res = await fetch('https://randomuser.me/api')
+    const data = await res.json()
+    const user = data.results[0]
+
+    const newUser = {
+        name: `${ user.name.first } ${ user.name.last }`,
+        money: Math.floor(Math.random() * 1000000)
+    }
+
+    addData(newUser)
+}
+
+function addData(obj) {
+    data.push(obj)
+
+    updateDOM()
+}
+
+function updateDOM(provideData = data) {
+    main.innerHTML = '<h2><strong>Person</strong>Wealth</h2>'
+    provideData.forEach(item => {
+        const element = document.createElement('div')
+        element.classList.add('person')
+        element.innerHTML = `<strong>${item.name}</strong> ${item.money}`
+        main.appendChild(element)
+    })
+
+}
+
+function formatMoney() {
+}
